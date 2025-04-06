3b7bcba8d314cfa71297d01bfe7fc515eeb78c71
Mauro Lafuente <155067361+MauroLaf@users.noreply.github.com>
2025-04-06 20:55:58 +0200

    comente la conexion con docker ya que no utilizare

--- Gym/Program.cs
+++ Gym/Program.cs
@@ -1,15 +1,20 @@
 using Gym.Models;
 using Gym.Services;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configura el DbContext con SQL Server
+// Configura el DbContext con db
+
 var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.23")));
 
 // 1. Servicios
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    options.UseMySql(connectionString, ServerVersion.AutoDetect("defaultConnection")));
 
 // 1.2. Registro del servicio UsuarioService
 builder.Services.AddScoped<UsuarioService>();
